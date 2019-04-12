fae4eb177ed23a25b10dde2a7cbee87de039cfb0
sdurandisse3 <stephaniedurandisse@pursuit.org>
2019-04-12 01:06:23 -0400

    fixed button position

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,28 +6,28 @@ import '../css/header.css'
 class Header extends Component {
     render() {
 
-    
-    return (
-        <div>
-             {/* <Link to='/'>Home</Link>
+
+        return (
+            <div className='parent'>
+                {/* <Link to='/'>Home</Link>
            <Link to='/login'>Connect to Spotify</Link>
            <Link to='/search'>Search</Link>
            <Link to='/signup'>Signup</Link>  */}
-           {/* <div className='background-img'>
+                {/* <div className='background-img'>
            <a href='spotify.com'>Connect to Spotify</a>
            </div> */}
 
-           <button>
-     <img src="/assets/Spotify_Icon_RGB_Black.png" className='background-img'alt=""></img>
-    Submit
-    </button>
+                <button className='btn'>
+                    <img src="/assets/Spotify_Icon_RGB_Black.png" className='background-img' alt=""></img>
+                    Submit
+                </button>
 
-        {/* <Button outline color="success" size="lg" style={{float: 'right'}}>Connect to Spotify</Button>{' '} */}
+                {/* <Button outline color="success" size="lg" style={{float: 'right'}}>Connect to Spotify</Button>{' '} */}
 
-       
-        </div>
-    )
-}
+
+            </div>
+        )
+    }
 
 }
 
